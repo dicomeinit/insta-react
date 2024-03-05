b98b48d0e1a89a8ee8aaeb77ac50dcc463f32628
Diana <dicomeinit@gmail.com>
2024-03-05 16:17:37 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import Post from "./Post";
+import {Avatar, Button} from 'antd';
 
 const BASE_URL = 'http://127.0.0.1:8001/'
 
 function App() {
 
     const [posts, setPosts] = useState([]);
+    const [openSignIn, setOpenSignIn] = useState(false);
+    const [openSignUp, setOpenSignUp] = useState(false);
 
     useEffect(() => {
         fetch(BASE_URL + 'post/all')
@@ -19,6 +22,16 @@ function App() {
                 throw response
             })
             .then(data => {
+                const result = data.sort((a, b) => {
+                    const t_a = a.timestamp.split(/[-T:]/);
+                    const t_b = b.timestamp.split(/[-T:]/);
+                    const d_a = new Date(Date.UTC(t_a[0], t_a[1]-1, t_a[2], t_a[3], t_a[4], t_a[5]));
+                    const d_b = new Date(Date.UTC(t_b[0], t_b[1]-1, t_b[2], t_b[3], t_b[4], t_b[5]));
+                    return d_b - d_a
+                })
+                return result
+            })
+            .then(data => {
                 setPosts(data)
             })
             .catch(error => {
@@ -27,12 +40,27 @@ function App() {
             })
     }, [])
     return (
-        <div className="app_posts">
-            {
-                posts.map(post => (
-                    <Post key={post.id} post={post}/>
-                ))
-            }
+        <div className="app">
+            <div className="app_header">
+                <img className="app_headerImage"
+                     src="https://cdn.icon-icons.com/icons2/2699/PNG/512/instagram_logo_icon_170643.png"
+                     alt="Instagram"/>
+                <div>
+                <Button type="submit" onClick={() => setOpenSignIn(true)}>Login</Button>
+                </div>
+                <div>
+                    <Button type="submit" onClick={() => setOpenSignUp(true)}>SignUp</Button>
+                </div>
+            </div>
+
+
+            <div className="app_posts">
+                {
+                    posts.map(post => (
+                        <Post key={post.id} post={post}/>
+                    ))
+                }
+            </div>
         </div>
 )
     ;
